577eb43cad618fad20bf1ee0c53cd1fa965fc66e
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-07 13:16:43 -0500

    try statement added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,5 +4,9 @@ document.querySelector("#currentyear").innerHTML = new Date().getFullYear();
 //set the date when the page was last updated
 const lastUpdated = new Date(document.lastModified);
 //set 24 hour format using options
+try{
 const options = { hour12: false };
 document.querySelector("#lastupdated").innerHTML = `<span class="highlight">Last updated: ${lastUpdated.toLocaleDateString("en-US", options)} ${lastUpdated.toLocaleTimeString("en-US", options)}<span class="highlight">`;
+}catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
