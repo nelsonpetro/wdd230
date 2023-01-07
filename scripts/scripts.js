8e53cad5a4b60d1f358f45c96faa4d7b1da0f39c
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-01-06 20:00:40 -0500

    images, styles and script foders added. Welcome portal completed

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+//set the current year at the footer
+document.querySelector("#currentyear").innerHTML = new Date().getFullYear();
+
+//set the date when the page was last updated
+const lastUpdated = new Date(document.lastModified);
+//set 24 hour format using options
+const options = { hour12: false };
+document.querySelector("#lastupdated").innerHTML = `<span class="highlight">Last updated: ${lastUpdated.toLocaleDateString("en-US", options)} ${lastUpdated.toLocaleTimeString("en-US", options)}<span class="highlight">`;
