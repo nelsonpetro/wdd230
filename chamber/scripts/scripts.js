8b15397c116af527b556bf9f922d9dab7f404227
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-02-24 01:30:14 -0500

    form added

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -56,9 +56,20 @@ document.querySelector("#days-visits").innerHTML = `Days since your last visit: 
 localStorage.setItem('lastVisitDate', currentDate.toString());
 
 
+const bposition = document.getElementById("pattern");
 
+bposition.addEventListener("input", (event) => {
+  if (bposition.validity.patternMismatch) {
+    bposition.setCustomValidity("Please enter at least 7 alphabetical characters, hyphens, or spaces.");
+  } else {
+    bposition.setCustomValidity("");
+  }
+});
 
-
+document.addEventListener("DOMContentLoaded", () => {
+    let hiddenField = document.getElementById("appdate");
+    hiddenField.value = new Date().toISOString();
+});
 
 
 
