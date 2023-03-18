8843bd65c033140e44c21de615ca9fc462544975
nelsonpetro <nelsonpetrodoria@gmail.com>
2023-03-18 10:59:04 -0500

    changes

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -13,7 +13,7 @@ document.querySelector("#lastupdated").innerHTML = `<span class="highlight">Last
 function toggleMenu() {
 	document.getElementById("primarynav").classList.toggle("open");
 	document.getElementById("hamburgerbtn").classList.toggle("open");
-	document.getElementById("pic").classList.toggle("open");
+	document.querySelector(".pic").classList.toggle("open");
 	document.getElementById("head").classList.toggle("open");
 	document.getElementById("socmedia").classList.toggle("open");
 	document.getElementById("header").classList.toggle("open");
@@ -54,67 +54,6 @@ if (lastVisitDate) {
 document.querySelector("#days-visits").innerHTML = `Days since your last visit:  ${daysDiff}`;
 
 localStorage.setItem('lastVisitDate', currentDate.toString());
-
-async function getData() {
-    const response = await fetch('data.json');
-    const data = await response.json();
-    console.log(data);
-    displaySpotlight(data);
-}
-
-getData();
-
-const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const displaySpotlight = (data) => {
-	const membership = data.filter(business => business.mlevel == "Gold" || business.mlevel == "Silver");
-	const randomInt = random(2,4);
-	const randSpotlights = membership.sort(() => .5 - Math.random()).slice(0,randomInt);
-
-	console.log(randomInt);
-
-	const container = document.querySelector(".spotlight-container");
-
-	randSpotlights.forEach((business) => {
-		let card = document.createElement('section');
-        let name = document.createElement('h2');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let membership = document.createElement('p');
-        let url = document.createElement('a');
-        let logo = document.createElement('img');
-        
-		card.setAttribute("class","spotlight");
-
-		const mquery = window.matchMedia("(min-width: 64rem)");
-
-		if(randomInt === 3 && mquery.matches) {
-			logo.style.maxWidth = "200px";
-		}
-		
-        name.textContent = `${business.name}`;
-        address.textContent = `${business.address}`;
-        phone.textContent = `${business.phone}`;
-        membership.textContent = `Membership level: ${business.mlevel}`;
-        url.textContent =`${business.url}`;
-        url.setAttribute('href', business.url);
-        url.setAttribute('target', '_blank');
-
-		logo.setAttribute('src', business.logo);
-        logo.setAttribute('alt', `Logo of ${business.name}`);
-        logo.setAttribute('loading', 'lazy');
-
-		card.appendChild(logo);
-		card.appendChild(name);
-		card.appendChild(address);
-		card.appendChild(phone);
-		card.appendChild(membership);
-		card.appendChild(url);
-
-		container.appendChild(card);
-	});
-
-}
 
 // Get all the links in the nav bar
 const navLinks = document.querySelectorAll('.nav-link');
